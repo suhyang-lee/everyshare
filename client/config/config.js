f01e22d00add776f9f143e9fd305b5a80e609ae1
suhyang-lee <suhyang.dev@gmail.com>
2021-03-04 22:24:32 +0900

    [update] add default setting

--- client/config/config.js
+++ client/config/config.js
@@ -1,5 +1,5 @@
 const closureURL = () => {
-  const url = "https://everyshare-server.herokuapp.com";
+  const url = "http://15.164.98.155";
   //const url = "http://localhost:3060";
   return {
     getServerURL: () => url,
