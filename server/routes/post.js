c03d84ee5c9508f913d25d540b38b839d2aa808c
suhyang-lee <suhyang.dev@gmail.com>
2021-03-05 05:19:39 +0900

    [update] multer setting

--- server/routes/post.js
+++ server/routes/post.js
@@ -28,7 +28,7 @@ const upload = multer({
     s3: new AWS.S3(),
     bucket: "everyshare",
     key(req, file, cb) {
-      cb(null, `origin/${Date.now()}_${path.basename(file.originalname)}`);
+      cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
     },
   }),
   limits: { fileSize: 20 * 1024 * 1024 },
